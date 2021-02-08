ed36f1d9be163feb10859c0261647823e1fec052
Voodstr <voods.work@yandex.ru>
2021-02-08 16:55:13 +0300

    Добавлена поддержка русского языка и диалог при выходе

--- app/src/main/java/ru/voodster/otuslesson/MainActivity.kt
+++ app/src/main/java/ru/voodster/otuslesson/MainActivity.kt
@@ -1,12 +1,10 @@
 package ru.voodster.otuslesson
 
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,6 +12,8 @@ import ru.voodster.otuslesson.film.FilmAdapter
 
 
 class MainActivity : AppCompatActivity() {
+
+    private var  backPressedTime:Long = 0
 
 
     private val recyclerView by lazy {
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initRecycler(){
-        val layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         recyclerView.layoutManager = layoutManager
             recyclerView.adapter = FilmAdapter(FilmList)
 
@@ -44,9 +44,32 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, FavoriteFilmsActivity()::class.java)
             startActivity(intent)
         }
-
     }
 
+   /* override fun onBackPressed() {
+        val t = System.currentTimeMillis()
+        if(t - backPressedTime > 2000){
+            backPressedTime = t
+            Toast.makeText(this, "Press back again to logout",
+                Toast.LENGTH_SHORT).show()
+        } else {    // this guy is serious
+            // clean up
+            super.onBackPressed();       // bye
+        }
+    }*/
 
 
+    override fun onBackPressed() {
+        val exitDialogBuilder = AlertDialog.Builder(this)
+        exitDialogBuilder.setTitle(R.string.exitDialogTitle)
+        exitDialogBuilder.setMessage(R.string.exitDialogText)
+        exitDialogBuilder.setCancelable(true)
+        exitDialogBuilder.setPositiveButton(R.string.yesBtn
+        ) { _, _ ->
+            super.onBackPressed()
+        }
+        val b = exitDialogBuilder.create()
+        b.show()
+    }
 }
+
