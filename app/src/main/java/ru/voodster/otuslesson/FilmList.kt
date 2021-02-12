386b41c77b83e73ed1617d86770eeaf28645d274
Voodstr <voods.work@yandex.ru>
2021-02-12 22:23:35 +0300

    1. Title не должен вылезать теперь 2. Обновлен список фильмов

--- app/src/main/java/ru/voodster/otuslesson/FilmList.kt
+++ app/src/main/java/ru/voodster/otuslesson/FilmList.kt
@@ -47,64 +47,64 @@ object FilmList: ArrayList<FilmItem>() {
             FilmItem(
             3,
             R.drawable.ic_inbruges,
-            "In Bruges",
-            "Чёрная трагикомедия режиссёра и сценариста Мартина Макдонаха 2008 года.")
+            "The Shawshank Redemption",
+            "Фильм основанный на повести Стивена Кинга «Рита Хейуорт и спасение из Шоушенка».")
         )
         this.add(
             FilmItem(
             4,
-            R.drawable.ic_inception,
-            "Inception",
-            "Научно-фантастический триллер Кристофера Нолана, основанный на идее осознанных сновидений")
+            R.drawable.ic_batman,
+            "The Dark Knight",
+            "Супергеройский боевик с элементами неонуара режиссёра Кристофера Нолана.")
         )
         this.add(
             FilmItem(
             5,
-            R.drawable.ic_lotr,
-            "Lord of the rings",
-            "Экранизация романа Дж. Р. Р. Толкина «Властелин колец»")
+            R.drawable.ic_godfather,
+            "The Godfather",
+            "Эпическая гангстерская драма режиссёра Фрэнсиса Форда Копполы.")
         )
         this.add(
             FilmItem(
             6,
-            R.drawable.ic_inbruges,
-            "In Bruges",
-            "Чёрная трагикомедия режиссёра и сценариста Мартина Макдонаха 2008 года")
+            R.drawable.ic_pulp,
+            "Pulp Fiction",
+            "Мультивселенный криминальный мир от режиссера Квентина Тарантино")
         )
         this.add(
             FilmItem(
             7,
-            R.drawable.ic_inception,
-            "Inception",
-            "Научно-фантастический триллер Кристофера Нолана, основанный на идее осознанных сновидений")
+            R.drawable.ic_fight,
+            "Fight Club",
+            "Кинофильм режиссёра Дэвида Финчера по мотивам одноимённого романа Чака Паланика")
         )
         this.add(
             FilmItem(
             8,
-            R.drawable.ic_lotr,
-            "Lord of the rings",
-            "Экранизация романа Дж. Р. Р. Толкина «Властелин колец»")
+            R.drawable.ic_forrest,
+            "Forrest Gump",
+            "Драма, девятый полнометражный фильм режиссёра Роберта Земекиса.")
         )
         this.add(
             FilmItem(
             9,
-            R.drawable.ic_inbruges,
-            "In Bruges",
-            "Чёрная трагикомедия режиссёра и сценариста Мартина Макдонаха 2008 года")
+            R.drawable.ic_matrix,
+            "Matrix",
+            "Американо-авcтpалийcкий научно-фантастический боевик, снятый братьями Вачовски.")
         )
         this.add(
             FilmItem(
             10,
-            R.drawable.ic_inception,
-            "Inception",
-            "Научно-фантастический триллер Кристофера Нолана, основанный на идее осознанных сновидений")
+            R.drawable.ic_cucuwka,
+            "One Flew Over the Cuckoo’s Nest",
+            "Художественный фильм-драма кинорежиссёра Милоша Формана")
         )
         this.add(
             FilmItem(
             11,
-            R.drawable.ic_lotr,
-            "Lord of the rings",
-            "Экранизация романа Дж. Р. Р. Толкина «Властелин колец»")
+            R.drawable.ic_lambs,
+            "The Silence of the Lambs",
+            "Американский триллер 1991 года режиссёра Джонатана Демми, снятый по мотивам одноимённого романа Томаса Харриса")
         )
     }
 
