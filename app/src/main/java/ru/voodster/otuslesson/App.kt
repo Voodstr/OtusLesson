2532ab4291a515b5673d371efcc439680a405a4f
Voodstr <voods.work@yandex.ru>
2021-06-16 19:49:16 +0300

    api for db.shs

--- app/src/main/java/ru/voodster/otuslesson/App.kt
+++ app/src/main/java/ru/voodster/otuslesson/App.kt
@@ -33,13 +33,9 @@ class App:Application() {
 
     private fun initDatabase() {
         Log.d(TAG, "initDatabase")
-
-
         Executors.newSingleThreadScheduledExecutor().execute(
-            Runnable {
+            {
                 Db.getInstance()?.getFilmsDao()?.getInitial()
-                suspend {
-                }
             }
         )
     }
@@ -73,9 +69,9 @@ class App:Application() {
         filmsUpdater = FilmsUpdater(filmsApi)
         Log.d(TAG,"success")
     }
-//"http://10.0.2.2/"
+
     companion object{
-        const val BASE_URL = "http://130.193.43.126/"
+        const val BASE_URL = "https://db.shs.com.ru/"
 
         const val TAG = "App"
 
